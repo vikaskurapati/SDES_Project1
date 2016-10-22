e5dbfb41e1119919b5cdb51da61c40bae5c4da06
Vikas Kurapati <vikaskurapati@iitb.ac.in>
2016-10-22 03:35:47 +0000

    Added more in documentation and modified the code included animation saving.

--- Project1_130010058.py
+++ Project1_130010058.py
@@ -14,26 +14,25 @@ from matplotlib import animation
 
 # In[3]:
 
-def problem(C,L,R,V,V0,I0,tf,n):
+def problem(C, L, R, V, V0, I0, tf, n):
     det = ((R*R)/(L*L)) - (4/(L*C)) + 0.0j
-    t = np.linspace(0.,tf,n)
+    t = np.linspace(0., tf, n)
     if abs(det) == 0:
         D1 = (I0/C)*np.exp(1/(2.0*R*C))
         D2 = (V0 - V)*np.exp(1/(2.0*R*C))
-        V_matrix = V + np.exp(-(1/2.0*R*C)*t)*(D1*t + D2) 
-    else :
+        V_matrix = V + np.exp(-(1/2.0*R*C)*t)*(D1*t + D2)
+    else:
         s1 = -0.5*R/L + 0.5*np.sqrt(det)
         s2 = -0.5*R/L - 0.5*np.sqrt(det)
         A1 = ((I0/C) - (V0 - V)*s2)/(s1 - s2)
         A2 = ((I0/C) - (V0 - V)*s1)/(s2 - s1)
         V_matrix = V + A1*np.exp(s1*t) + A2*np.exp(s2*t)
-    return V_matrix,t
-
-
+    return V_matrix, t
 
 # In[4]:
 
-def plot(t,V):
+
+def plot(t, V):
     plt.plot(t,V)
     plt.show()
 
@@ -56,13 +55,13 @@ def animate(X,Y,name):
         #print x,y
         line.set_data(x, y)
         return line,
-    
+
     anim = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=200, interval=20, blit=True)
     anim.save(name+'.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
     plt.show()
 
-    
+
 
 
 # In[6]:
@@ -71,4 +70,3 @@ if __name__ == '__main__':
     V,t = problem(1.0, 1.0, 1.0, 10.0, 10.0, 1.0, 20.0, 200)
     plot(t,V)
     animate(t,V,'Voltage')
-
