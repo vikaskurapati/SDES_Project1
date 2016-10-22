2a0d86d899606182f1b3483cfd9ca0bb3afd1868
Vikas Kurapati <vikaskurapati@iitb.ac.in>
2016-10-22 13:57:45 +0000

    IPython notebook working with animations and plots. Script working. Make file left.

--- Project1_130010058.py
+++ Project1_130010058.py
@@ -1,49 +1,108 @@
 
 # coding: utf-8
 
-## SDES Project1: LC Tank with a resistance(constant voltage applied)
+# # SDES Project1: LC Tank with a resistance(constant voltage applied)
 
-# In[2]:
+# In[94]:
+
+#get_ipython().magic(u'pylab inline')
+
+
+# In[95]:
 
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
+from tempfile import NamedTemporaryFile
+from IPython.display import HTML
 
 
-####### C is capacitance of the capacitor, L is inducatance of inductor, R is resistor's resistance,V is the voltage applied, $V_0$ is initial voltage across capacitor and $I_0$ being initial current across the inductor, $t_f$ is the total time and n is the number of time steps
+# ### C is capacitance of the capacitor, L is inducatance of inductor, R is resistor's resistance,V is the voltage applied, $V_0$ is initial voltage across capacitor and $I_0$ being initial current across the inductor, $t_f$ is the total time and n is the number of time steps
 
-# In[3]:
+# In[96]:
 
-def problem(C, L, R, V, V0, I0, tf, n):
+def problem(C,L,R,V,V0,I0,tf,n):
     det = ((R*R)/(L*L)) - (4/(L*C)) + 0.0j
-    t = np.linspace(0., tf, n)
+    t = np.linspace(0.,tf,n)
     if abs(det) == 0:
         D1 = (I0/C)*np.exp(1/(2.0*R*C))
         D2 = (V0 - V)*np.exp(1/(2.0*R*C))
         V_matrix = V + np.exp(-(1/2.0*R*C)*t)*(D1*t + D2)
-    else:
+        i_matrix = C*np.exp(-t/(2.0*R*C))*(D1 - (D1*t + D2)/(2.0*R*C)) 
+    else :
         s1 = -0.5*R/L + 0.5*np.sqrt(det)
         s2 = -0.5*R/L - 0.5*np.sqrt(det)
         A1 = ((I0/C) - (V0 - V)*s2)/(s1 - s2)
         A2 = ((I0/C) - (V0 - V)*s1)/(s2 - s1)
         V_matrix = V + A1*np.exp(s1*t) + A2*np.exp(s2*t)
-    return V_matrix, t
-
-# In[4]:
-
-
-def plot(t, V):
-    plt.plot(t,V)
-    plt.show()
+        i_matrix = C*(A1*s1*np.exp(s1*t) + A2*s2*np.exp(s2*t))
+    return V_matrix,i_matrix,t
 
 
-# In[ ]:
+# #### The results are plotted showing the voltage across the capacitor varying with time for three cases of damping
+
+# In[97]:
+
+V_u,i_u,t_u = problem(1.0, 1.0,0.5, 10.0, 10.0, 1.0, 25.0, 250)
+V_c,i_c,t_c = problem(1.0, 1.0,2.0, 10.0, 10.0, 1.0, 25.0, 250)
+V_o,i_o,t_o = problem(1.0, 1.0,4.5, 10.0, 10.0, 1.0, 25.0, 250)
+plt.figure(1)
+plt.plot(t_u,V_u,label = 'Under damped')
+plt.plot(t_c,V_c,label = 'Critically damped')
+plt.plot(t_o,V_o,label = 'Over damped')
+plt.title('Voltage of capacitor with time')
+plt.ylabel('Capacitor Voltage')
+plt.xlabel('time')
+plt.legend()
+plt.savefig('VC.png')
+plt.show()
 
 
+# #### The results were plotted showing variation of voltage across the capacitor, inductor and resistor for an underdamped case of R = 0.5.
+
+# In[98]:
+
+R = 0.5
+V = 10.0
+V_u,i_u,t_u = problem(1.0, 1.0,R, 10.0, 0.0, 0.0, 25.0, 250)
+plt.plot(t_u,V_u,label = 'Capacitor')
+plt.plot(t_u,R*i_u,label = 'Resistor')
+plt.plot(t_u, V - V_u - R*i_u ,label = 'inductor')
+plt.xlabel('time')
+plt.ylabel('Voltage across components')
+plt.legend(loc = 'lower right')
+plt.savefig('V.png')
+plt.show()
+
+# ### The code required for animation.
+
+# In[99]:
+
+'''
+VIDEO_TAG = """<video controls>
+     <source src="data:video/x-m4v;base64,{0}" type="video/mp4">
+     Your browser does not support the video tag.
+    </video>"""
+
+def anim_to_html(anim):
+    if not hasattr(anim, '_encoded_video'):
+        with NamedTemporaryFile(suffix='.mp4') as f:
+            anim.save(f.name, fps=20, extra_args=['-vcodec', 'libx264'])
+            video = open(f.name, "rb").read()
+        anim._encoded_video = video.encode("base64")
+
+    return VIDEO_TAG.format(anim._encoded_video)
+
+def display_animation(anim):
+    plt.close(anim._fig)
+    return HTML(anim_to_html(anim))
+
+'''
+# In[100]:
 
 def animate(X,Y,name):
     fig = plt.figure()
-    ax = plt.axes(xlim=(np.amin(X), np.amax(X)+0.1), ylim=(np.amin(Y)-0.1, np.amax(Y)+0.1))
+    ax = plt.axes(xlim=(np.amin(X), np.amax(X)), ylim=(np.amin(Y)-0.1, np.amax(Y)+0.1))
     line, = ax.plot([], [], lw=2)
 
     def init():
@@ -52,21 +111,24 @@ def animate(X,Y,name):
     def animate(i):
         x = X[:i]
         y = Y[:i]
-        #print x,y
         line.set_data(x, y)
         return line,
 
     anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=20, blit=True)
-    anim.save(name+'.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+                               frames=250, interval=5, blit=True)
+    anim.save(name+'.mp4', fps=120, extra_args=['-vcodec', 'libx264'])
+
     plt.show()
 
 
+# ### The following animation is showing the variation of potential difference across capacitor with time in an underdamped case with R = 0.5.  Please click the play button in the input for the animation to be played.
+
+# In[101]:
+
+animate(t_u,V_u,'vcu')
 
 
-# In[6]:
+# ### The following animation shows the variation of current in the circuit with time.
 
-if __name__ == '__main__':
-    V,t = problem(1.0, 1.0, 1.0, 10.0, 10.0, 1.0, 20.0, 200)
-    plot(t,V)
-    animate(t,V,'Voltage')
+# In[102]:
+animate(t_u,i_u,'iu')
